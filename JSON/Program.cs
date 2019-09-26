c71247db70ae60a8fd95347bb2381330bef5d15c
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-26 19:51:22 +0200

    Add project files.

---
+++ JSON/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository repo = new Repository();
+            //repo.Add(new Person
+            //{
+            //    LastName = "LN",
+            //    Name = "N",
+            //    ListOfObjects = new List<string> {"1","2"}
+            //});
+            //var list = repo.GetList();
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            repo.Delete("N");
+            Console.WriteLine(repo.GetList());
+
+
+
+
+
+
+
+
+            var data = File.ReadAllText(ConfigurationManager.AppSettings["file"]);
+            JObject jsonObject = JObject.Parse(data);
+            Console.WriteLine(jsonObject["name"]);
+            var person = JsonConvert.DeserializeObject<List<Person>>(data);
+            person.RemoveAt(0);
+
+            string dataToSave = JsonConvert.SerializeObject(person);
+            File.WriteAllText(ConfigurationManager.AppSettings["file"],
+                JToken.Parse(dataToSave).ToString());
+            ////Console.WriteLine(person[0]);
+            ////Console.WriteLine(person[1]);
+            Console.Read();
+        }
+    }
+}
---
+++ JSON/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    public class Repository
+    {
+        private string file = ConfigurationManager.AppSettings["file"];
+        private List<Person> getPersonList
+        {
+            get
+            {
+                var data = File.ReadAllText(file);
+                return JsonConvert.DeserializeObject<List<Person>>(data);
+            }
+        }
+        private void serialize(List<Person> person)
+        {
+            string dataToSave = JsonConvert.SerializeObject(person);
+            File.WriteAllText(ConfigurationManager.AppSettings["file"],
+                JToken.Parse(dataToSave).ToString());
+        }
+
+        internal void Delete(string name)
+        {
+            var personlist = getPersonList;
+            personlist.Remove(personlist.FirstOrDefault(x=>x.Name == name));
+            serialize(personlist);
+        }
+
+        internal void Add(Person person)
+        {
+            var data = File.ReadAllText(file);
+            var personlist = getPersonList;
+            personlist.Add(person);
+
+            
+        }
+        internal List<Person> GetList()
+        {
+            var data = File.ReadAllText(file);
+            var personlist = getPersonList;
+            
+        }
+    }
+}
